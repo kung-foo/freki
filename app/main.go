b481936dc058625e63d53b977a771660615b9a7e
kung-foo <github@jonathan.camp>
2017-01-16 20:38:31 +0100

    add doc-opt, add new interface logic, add tcp logger

--- app/main.go
+++ app/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
-	"encoding/hex"
-	"fmt"
-	"net"
-	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
+	docopt "github.com/docopt/docopt-go"
 	"github.com/kung-foo/freki"
 	log "github.com/sirupsen/logrus"
 )
+
+// VERSION is set by the makefile
+var VERSION = "0.0.0"
+
+var usage = `
+Usage:
+    freki [options] [-v ...] -i=<interface>
+    freki -h | --help | --version
+Options:
+    -i --interface=<iface>  Bind to this interface.
+    -h --help               Show this screen.
+    --version               Show version.
+    -v                      Enable verbose logging (-vv for very verbose)
+`
+
+func main() {
+	mainEx(os.Args[1:])
+}
 
 func onErrorExit(err error) {
 	if err != nil {
@@ -29,8 +44,10 @@ func onInterruptSignal(fn func()) {
 	}()
 }
 
-func main() {
-	var err error
+func mainEx(argv []string) {
+	args, err := docopt.Parse(usage, argv, true, VERSION, true)
+	onErrorExit(err)
+
 	// log.SetLevel(log.DebugLevel)
 	logger := log.New()
 	logger.Level = log.DebugLevel
@@ -41,7 +58,10 @@ func main() {
 	rules, err := freki.ReadRulesFromFile(rulesFile)
 	onErrorExit(err)
 
-	processor := freki.New(rules, logger)
+	processor, err := freki.New(args["--interface"].(string), rules, logger)
+	onErrorExit(err)
+
+	processor.AddServer(freki.NewTCPLogger(6000, 1024))
 
 	err = processor.Init()
 	onErrorExit(err)
@@ -64,43 +84,48 @@ func main() {
 		}
 	}()
 
-	// TODO: move
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		host, port, _ := net.SplitHostPort(r.RemoteAddr)
-		log.Infof("http %s %s", host, port)
-		ck := freki.NewConnKeyByString(host, port)
-		md := processor.Connections.GetByFlow(ck)
-		//fmt.Fprintf(w, "Hello on port %d\n", md.TargetPort)
-		log.Infof("%s -> %s", host, md.TargetPort.String())
-		fmt.Fprintf(w, "OK")
-	})
+	/*
+		// TODO: move
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			host, port, _ := net.SplitHostPort(r.RemoteAddr)
+			log.Infof("http %s %s", host, port)
+			ck := freki.NewConnKeyByString(host, port)
+			md := processor.Connections.GetByFlow(ck)
+			//fmt.Fprintf(w, "Hello on port %d\n", md.TargetPort)
+			log.Infof("%s -> %s", host, md.TargetPort.String())
+			fmt.Fprintf(w, "OK")
+		})
 
-	go http.ListenAndServe(":8080", nil)
 
-	go func() {
-		ln, err := net.Listen("tcp", ":6000")
-		onErrorExit(err)
+		go http.ListenAndServe(":8080", nil)
+	*/
 
-		for {
-			conn, err := ln.Accept()
+	/*
+		go func() {
+			ln, err := net.Listen("tcp", ":6000")
 			onErrorExit(err)
 
-			go func(conn net.Conn) {
-				conn.SetReadDeadline(time.Now().Add(time.Second * 5))
-				host, port, _ := net.SplitHostPort(conn.RemoteAddr().String())
-				ck := freki.NewConnKeyByString(host, port)
-				md := processor.Connections.GetByFlow(ck)
-				log.Infof("%s -> %s", host, md.TargetPort)
-				buffer := make([]byte, 1024)
-				n, _ := conn.Read(buffer)
-				hex.Dumper(log.StandardLogger().Writer()).Write(buffer[0:n])
-				err := conn.Close()
-				if err != nil {
-					log.Error(err)
-				}
-			}(conn)
-		}
-	}()
+			for {
+				conn, err := ln.Accept()
+				onErrorExit(err)
+
+				go func(conn net.Conn) {
+					conn.SetReadDeadline(time.Now().Add(time.Second * 5))
+					host, port, _ := net.SplitHostPort(conn.RemoteAddr().String())
+					ck := freki.NewConnKeyByString(host, port)
+					md := processor.Connections.GetByFlow(ck)
+					log.Infof("%s -> %s", host, md.TargetPort)
+					buffer := make([]byte, 1024)
+					n, _ := conn.Read(buffer)
+					hex.Dumper(log.StandardLogger().Writer()).Write(buffer[0:n])
+					err := conn.Close()
+					if err != nil {
+						log.Error(err)
+					}
+				}(conn)
+			}
+		}()
+	*/
 
 	// TODO: pass in stop channel
 	processor.Start()
